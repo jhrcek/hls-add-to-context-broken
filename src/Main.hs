1d3bbdf77d5b0609c58069923baab7ee713b98d6
Jan Hrček <honza.hrk@gmail.com>
2020-09-22 15:34:27 +0200

    Simplify and add reproducer

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,28 @@
 module Main where
 
+import Control.Monad.IO.Class (MonadIO (liftIO))
+
+-- REPRODUCER 1:
+-- 1. Hover over liftIO
+-- 2. Click "lightbulb" and select "Add `MonadIO m` to the context of the type signature for `a`"
+
+-- Buggy behavior 1: syntactically malformed signature is created: "a :: (Monad m, MonadIO m)m) => m ()"
+a :: (Monad m) => m ()
+a = liftIO (pure ())
+
+
+-- REPRODUCER 2 - note that the signature begins on the next line than the symbol being annotated:
+-- 1. Hover over liftIO
+-- 2. Click "lightbulb" and select "Add `MonadIO m` to the context of the type signature for `a`"
+
+-- Buggy behavior 2: The new context is added at the end of file (!)
+
+
+b
+   :: ( Monad m
+      )
+    =>  m ()
+b = liftIO $ pure ()
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = pure ()
